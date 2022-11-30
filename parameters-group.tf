4ad067d50e2d736e31da644d230696b6f52a7cb5
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-30 11:51:09 +0530

    updating ./terraform/tf-module-rds

---
+++ parameters-group.tf
@@ -0,0 +1,5 @@
+resource "aws_db_parameter_group" "main" {
+  name   = "${local.TAG_PREFIX}-pg"
+  family = var.PG_FAMILY
+
+}
---
+++ vars.tf
@@ -0,0 +1,11 @@
+// VPC
+variable "ENV" {}
+variable "PROJECT" {}
+variable "ENGINE" {}
+variable "ENGINE_VERSION" {}
+variable "RDS_INSTANCE_CLASS" {}
+variable "PG_FAMILY" {}
+variable "PRIVATE_SUBNET_IDS" {}
+variable "ALLOW_SG_CIDR" {}
+variable "VPC_ID" {}
+variable "RDS_PORT" {}
