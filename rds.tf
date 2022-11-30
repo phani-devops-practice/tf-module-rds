50e8673c2bc7ad24d12d4e6d62cc072c8e29ae5c
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-30 12:00:55 +0530

    updating ./terraform/tf-module-rds

--- rds.tf
+++ rds.tf
@@ -6,6 +6,7 @@ resource "aws_db_instance" "main" {
   username               = "foo"
   password               = "foobarbaz"
   parameter_group_name   = aws_db_parameter_group.main.name
+  db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.main.id]
   skip_final_snapshot    = true
   identifier             = local.TAG_PREFIX
