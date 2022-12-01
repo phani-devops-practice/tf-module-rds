090d12f9f0aa946f0ab2272f4ef6b07b89de68a6
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-12-01 11:56:08 +0530

    updating ./terraform/tf-module-rds

---
+++ data.tf
@@ -0,0 +1,11 @@
+data "aws_secretsmanager_secret" "secret" {
+  name  = "roboshop/all"
+}
+
+data "aws_secretsmanager_secret_version" "secret" {
+  secret_id = data.aws_secretsmanager_secret.secret.id
+}
+
+
+
+
--- rds.tf
+++ rds.tf
@@ -3,8 +3,8 @@ resource "aws_db_instance" "main" {
   engine                 = var.ENGINE
   engine_version         = var.ENGINE_VERSION
   instance_class         = var.RDS_INSTANCE_CLASS
-  username               = "foo"
-  password               = "foobarbaz"
+  username               = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]
+  password               = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]
   parameter_group_name   = aws_db_parameter_group.main.name
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.main.id]
